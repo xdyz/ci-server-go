2bbd9c8a44dc0728e693a5d3af1187dcd9c7b44c
zhanghaojie <zhj778930960@outlook.com>
2022-02-12 01:21:32 +0800

    <feat> 结构

--- main.go
+++ main.go
@@ -1,21 +1,5 @@
 package main
 
-import (
-	"github.com/gin-gonic/gin"
-)
-
-func Helo(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"message": "pong",
-	})
-}
-
 func main() {
-	// 返回 gin 引擎
-	e := gin.Default()
 
-	e.GET("/ping", Helo)
-
-	// port 端口为3000  如果不写默认为8080
-	e.Run(":3000")
 }
