cafd23219baf633b7daa6adfe51249e364e94238
zhanghaojie <zhj778930960@outlook.com>
2022-02-27 00:28:17 +0800

    <feat> orm 注释

--- model/UserEntity.go
+++ model/UserEntity.go
@@ -3,7 +3,7 @@ package model
 import "gorm.io/gorm"
 
 // 定义UserEntity结构体  设置字段各种属性，如主键，默认值，索引等，配置json格式名称 如果不需要json序列化，可以不设置
-
+// 这里的 json 设置就是 当接口查询时，返回给前端的 对象数据中 的属性 是什么
 type UserEntity struct {
 	gorm.Model
 	UserName string `gorm:"type:varchar(50);column:username;not null;unique;comment:'用户名'" json:"username"`
